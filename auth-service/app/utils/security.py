86abb99735291dcaeca8fbd501055ea5607c01d9
chathuranga836@gmail.com <chaturanga836@gmail.com>
2025-10-18 23:00:07 +0530

    pwslib added

--- auth-service/app/utils/security.py
+++ auth-service/app/utils/security.py
@@ -1,9 +1,10 @@
-from passlib.context import CryptContext
-
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+# app/utils/security.py
+from pwdlib.hash import bcrypt
 
 def hash_password(password: str) -> str:
-    return pwd_context.hash(password)
+    """Hash a password for storing in the database."""
+    return bcrypt.hash(password)
 
 def verify_password(plain_password: str, hashed_password: str) -> bool:
-    return pwd_context.verify(plain_password, hashed_password)
+    """Verify a stored password against one provided by user."""
+    return bcrypt.verify(plain_password, hashed_password)
