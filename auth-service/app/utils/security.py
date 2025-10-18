ddc4177d551b045fbeb7d3d6fb196c57d52a5c99
chathuranga836@gmail.com <chaturanga836@gmail.com>
2025-10-18 23:06:14 +0530

    update requiremnt .txt

--- auth-service/app/utils/security.py
+++ auth-service/app/utils/security.py
@@ -1,10 +1,13 @@
 # app/utils/security.py
-from pwdlib.hash import bcrypt
+from pwdlib import PasswordHash
+
+# Create a recommended hasher (defaults to Argon2)
+password_hasher = PasswordHash.recommended()
 
 def hash_password(password: str) -> str:
     """Hash a password for storing in the database."""
-    return bcrypt.hash(password)
+    return password_hasher.hash(password)
 
 def verify_password(plain_password: str, hashed_password: str) -> bool:
     """Verify a stored password against one provided by user."""
-    return bcrypt.verify(plain_password, hashed_password)
+    return password_hasher.verify(plain_password, hashed_password)
